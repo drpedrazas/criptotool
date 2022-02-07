007fccf63a3b8a961f8b4404659024393afd0d84
david <drpedrazas@unal.edu.co>
2022-02-06 22:17:26 -0500

    Started Hill

--- src/cryptotool.java
+++ src/cryptotool.java
@@ -1,8 +1,5 @@
 public class cryptotool {
     public static void main(String[] args){
-        Vigenere test = new Vigenere("ABSENTA");
-        System.out.println(test.encrypt("HELLO"));
-        System.out.println(test.decrypt(test.encrypt("HELLO")));
-        int[] numbers = {1,2};
+
     }
 }
